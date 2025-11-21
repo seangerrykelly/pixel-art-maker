aeb85e99e4af8c5ec589f82f6c280f117de83166
Sean Kelly <seangerrykelly@gmail.com>
2025-11-20 21:17:12 -0500

    chore: move edit grid modal into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css'
+import { EditGridModal } from '@/components/EditGridModal'
 import { PixelGrid } from '@/components/PixelGrid'
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { useState, type FormEvent } from 'react'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
 
 function App() {
 
@@ -23,34 +20,11 @@ function App() {
   return (
     <div className="bg-background flex justify-center w-screen h-screen">
       <div className="flex items-center">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button>Edit</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <form className="grid gap-4" onSubmit={handleSubmitEditGrid}>
-              <DialogHeader>
-                <DialogTitle>Edit Grid</DialogTitle>
-              </DialogHeader>
-              <div className="grid gap-4">
-                <div className="grid gap-3">
-                  <Label htmlFor="grid-height">Height</Label>
-                  <Input id="grid-height" name="height" defaultValue={gridHeight} type="number" min={1} required/>
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="grid-width">Width</Label>
-                  <Input id="grid-width" name="width" defaultValue={gridWidth} type="number" min={1} required/>
-                </div>
-              </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </form>
-          </DialogContent>
-        </Dialog>
+        <EditGridModal 
+          gridHeight={gridHeight} 
+          gridWidth={gridWidth} 
+          handleSubmit={handleSubmitEditGrid}
+        />
         <PixelGrid height={gridHeight} width={gridWidth} />
       </div>
     </div>
