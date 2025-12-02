db00ad8a5e98ece76a47aed47912f67d53750d21
Sean Kelly <seangerrykelly@gmail.com>
2025-12-01 19:32:30 -0500

    chore: change name to BitBuilder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
 
     return (
         <div className="p-1 shadow-md w-full flex items-center justify-between border">
-            <h1>Pixel Art Maker</h1>
+            <h1 className="text-xl">BitBuilder</h1>
             <div className="flex gap-2">
                 <Button onClick={toggleTheme}>Toggle theme</Button>
                 <ExportImage />
